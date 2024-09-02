a5b6eb42614dfa27ed866d085d99494acd601c51
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-02 11:01:05 +0000

    String intro

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,44 @@
+const name  = "pavan"
+const repoCount = 50
+//console.log(name+ repoCount+"value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+//const str = new String('Pavan');
+//console.log(str);
+
+console.log(name.length);
+console.log(name.toUpperCase());
+
+console.log(name.charAt(2));
+console.log(name.indexOf('n'));
+
+const newString = name.substring(0,3);
+console.log(newString);
+
+const anotherString = name.slice(-4,4);
+console.log(anotherString);
+
+const newstring1 = "   pavan   ";
+console.log(newstring1);
+console.log(newstring1.trim());
+
+const url = "https://pavan.com/pavan%20kumar"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('pavan'))
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
