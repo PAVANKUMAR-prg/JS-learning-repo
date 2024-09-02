c5c7876b75c527d72e169c9a16c10be2f81997f4
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-02 11:30:43 +0000

    Math and Num

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,51 @@
+const num = 400;
+// console.log(num);
+
+const num1 = new Number(100000000);
+// console.log(num1);
+
+// console.log(num1.toString().length);
+
+// console.log(num1.toFixed(2));
+// console.log(num1.toPrecision(3))
+
+// console.log(num1.toLocaleString('en-IN'));
+
+
+//-------------------maths---------------
+
+
+console.log(Math)
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.max(4,5,2,8,6,9));
+// console.log(Math.min(4,5,2,8,6,9));
+
+console.log((Math.random()*10) + 1);
+console.log((Math.floor(Math.random()*10)) + 10);
+console.log(Math.random());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
