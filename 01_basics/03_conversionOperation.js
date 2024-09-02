2bfb8cc70e5ac11cffb891e6654aa28453166ea7
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-02 09:53:17 +0000

    Datatypes summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,6 +23,37 @@ let booleanValue = Boolean(isLoggedIn)
 
 //Also mind need to write readable code that's it about operations 
 
+//--------------------DataTypes-------------------------------------
+/*Two types of DataTypes mainly
+primitive and non primitive(reference types)
+*/
+//Primitive types are- Number string Boolean Symbol, Null , Undefined BigInt
+
+const id = Symbol('1234')
+const tempId = Symbol('1234')
+console.log(id === tempId)
+;
+
+
+//Non primitive types are Array, Object and Function
+let arr = [1,2,3,4]
+let add = function()
+{
+    console.log("Heloo world");
+    
+}
+let exampleObj = {
+    name:"Pavan",
+    Age:20
+}
+
+
+console.log(arr);
+console.log(typeof arr);
+console.log(typeof add);
+console.log(typeof exampleObj);
+
+
 
   
 
