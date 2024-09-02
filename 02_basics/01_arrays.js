49e3f3cd77d1859cbc0beb318547ac23a3c60f63
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-02 12:08:56 +0000

    Array in JS

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,53 @@
+const arr1 = [1,2,3,4,5];
+// console.log(arr1);
+
+// console.log(arr1.length);
+
+
+//Array methods
+
+// arr1.push(6)
+// arr1.push(7)
+// arr1.pop()
+
+//arr1.unshift(1)
+//arr1.shift(1)
+
+// console.log(arr1.includes(9));
+// console.log(arr1.indexOf(9));
+
+// const arr2 = arr1.join()
+// console.log(arr1);
+// console.log(arr2);
+
+//slice , splice
+
+console.log("A ", arr1);
+console.log(arr1.slice(1,3));
+
+const arr2 = arr1.splice(1,3)
+console.log(arr2);
+console.log("A ", arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
