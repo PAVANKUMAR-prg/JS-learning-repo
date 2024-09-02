c151168bfa4d0c1b1810760bbf548a36cf80eb3a
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-02 12:34:56 +0000

    Arry's some methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,4 @@
-const arr1 = [1,2,3,4,5];
+// const arr1 = [1,2,3,4,5];
 // console.log(arr1);
 
 // console.log(arr1.length);
@@ -22,12 +22,44 @@ const arr1 = [1,2,3,4,5];
 
 //slice , splice
 
-console.log("A ", arr1);
-console.log(arr1.slice(1,3));
+// console.log("A ", arr1);
+// console.log(arr1.slice(1,3));
 
-const arr2 = arr1.splice(1,3)
-console.log(arr2);
-console.log("A ", arr1);
+// const arr2 = arr1.splice(1,3)
+// console.log(arr2);
+// console.log("A ", arr1);
+
+///////////////////////////////////////////////////
+
+ 
+const arr1 = [1,2,3,4,5]
+const arr2 = [6,7,8,9,10]
+
+// console.log(arr1);
+// console.log(arr2);
+
+const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+const arr4 = [...arr1,...arr2]
+// console.log(arr4);
+
+// console.log(Array.isArray("Pavan"));
+// console.log(Array.from("Pavan"));
+
+let s1 = 100
+let s2 = 200
+let s3 = 300
+ 
+console.log(Array.of(s1,s2,s3));
+
+
+
+
+
+
+
+
 
 
 
