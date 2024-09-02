60a639da0659a11807901ac317c05e5588e2a428
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-02 11:46:04 +0000

    Date and Time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+const date = new Date();
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.toLocaleDateString());
+
+//Little bit of complex with respect to Date and Time lots of different
+//ways to create and represent on the go we need to learn them.
+
