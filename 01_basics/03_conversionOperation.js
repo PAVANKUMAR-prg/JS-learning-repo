6a0655c20614703fa3c975e1f73dfd58b81140b1
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-02 06:13:30 +0000

    Conversion of datatypes discussed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = undefined
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+//"33" = 33
+//"33abc" = nan
+//true = 1; false = 0;
+
+let isLoggedIn = 0;
+let booleanValue = Boolean(isLoggedIn)
+console.log(booleanValue);
+
+//This is all about conversioning from one type to other 
+
+
