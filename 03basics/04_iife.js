414d139ae45a1f0a5942427aa62368e234a63498
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-17 06:04:52 +0000

    How JS code works

--- 03basics/04_iife.js
+++ 03basics/04_iife.js
@@ -11,3 +11,19 @@
 (()=>{
     console.log("Unnamed IIFE executing...")
 })();
+
+
+//Here lets discus about how js code executes
+/*
+When we run code first,
+1.Global Execution Constext or Global Environment creates,wher we represents code with "this".
+2.Next phase is Memory creation, all variables will be assigned to undefined,functions 
+will assigned or set with their definations untill next phase that is execution phase.
+3.Next phase is Execution phase where actuall values will assigne to varibles and function 
+call happens,for functions the memory phase and execution phase again happens just like above one. 
+
+need to keep in mind.
+1.global execution context.
+2.memory phase.
+3.execution phase.
+*/
