7dfa25a4ca4d1812cd092a988c3bfba044ecdca7
PAVANKUMAR BM <120642051+PAVANKUMAR-prg@users.noreply.github.com>
2024-09-17 16:36:29 +0000

    loops worked

---
+++ 05_iterations/one.js
@@ -0,0 +1,58 @@
+//for loop
+
+// for (let index = 0; index < 10; index++) 
+// {
+//         console.log(index);
+            
+// }
+
+for (let i = 1; i < 2; i++) {
+    for (let j = 1; j <= 10; j++) {
+        //console.log(i + `*` + j + `=` + i*j)
+    }    
+}
+
+let myArray = ['f', 'b', 's']
+//console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element)
+    
+}
+
+//while loop
+// x =1
+// while (x<10) {
+//     console.log(x)
+//     x++
+// }
+
+//do while loop
+
+// i = 1;
+// do {
+//     console.log(i);
+//     i++;
+    
+// } while (i<10);
+
+
+// for of loop specially to work with array
+
+const arr = [1,2,3,4,5]
+ for (const num of arr) {
+   // console.log(arr)
+ }
+ const greet = "Hello world"
+ for (const val of greet) {
+    //console.log(val);
+    
+ }
+
+
+
+ //Map
+
+ const map = new Map()
+ 
